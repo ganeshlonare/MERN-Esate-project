e8a415c35fb970ab5408c146a442bd2e32644765
ganeshlonare <ganeshlonare311@gmail.com>
2023-11-13 16:57:21 +0530

    create a middleware and a function to handle possible errors

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,10 +4,12 @@ const userSchema=new mongoose.Schema({
     username:{
         type:String,
         required:true,
+        unique:true,
     },
     email:{
         type:String,
         required:true,
+        unique:true,
     },
     password:{
         type:String,
