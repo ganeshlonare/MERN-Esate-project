016d6173c0a9f64f6d4f182741a4b503412e7544
ganeshlonare <ganeshlonare311@gmail.com>
2023-12-26 19:04:43 +0530

    Completed show user listings UI  & functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ export default function Profile() {
   const [formData , setFormData] = useState({});
   const dispatch=useDispatch();
   const [updateSuccess,setUpdateSuccess]=useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   
   useEffect(()=>{
     if(file) {
@@ -110,6 +112,22 @@ export default function Profile() {
      }
    }
 
+   const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-center text-3xl font-semibold my-7'>
@@ -135,6 +153,27 @@ export default function Profile() {
       </div>
       <p className='text-red-700'>{error? error : " "}</p>
       <p className='text-green-700'>{updateSuccess ? "User is updated successfully": " "}</p>
+      <button onClick={handleShowListings} className='w-full text-green-700'>Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt='listing cover' className='h-16 w-16 object-contain'/>
+              </Link>
+              <Link className='text-slate-700 font-semibold  hover:underline truncate flex-1' to={`/listing/${listing._id}`} >
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>}
     </div>
   )
 }
