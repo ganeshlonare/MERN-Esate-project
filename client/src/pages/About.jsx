6de3f977271159061f10d255386e05968cea15b1
ganeshlonare <ganeshlonare311@gmail.com>
2023-12-29 20:42:49 +0530

    Completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto p-28'>
+      <div>
+      <h1 className='mb-4 text-3xl font-bold text-slate-800'>About Real Estate</h1>
+    </div>
+    <div className="">
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora esse excepturi 
+        consectetur soluta natus necessitatibus sint cum delectus, quaerat et doloremque?
+         Fugiat ab aliquid commodi veritatis officia eum quia dignissimos id rerum. 
+         Inventore cum quod dignissimos impedit corrupti quam, fugit minus aliquam, 
+         incidunt culpa, laudantium sed itaque. Mollitia, distinctio ipsum.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora esse excepturi
+         consectetur soluta natus necessitatibus sint cum delectus, quaerat et doloremque?
+         Fugiat ab aliquid commodi veritatis officia eum quia dignissimos id rerum.
+         Inventore cum quod dignissimos impedit corrupti quam, fugit minus aliquam,
+         incidunt culpa, laudantium sed itaque. Mollitia, distinctio ipsum.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora esse excepturi
+         consectetur soluta natus necessitatibus sint cum delectus, quaerat et doloremque?
+         Fugiat ab aliquid commodi veritatis officia eum quia dignissimos id rerum. 
+        Inventore cum quod dignissimos impedit corrupti quam, fugit minus aliquam,
+         incidunt culpa, laudantium sed itaque. Mollitia, distinctio ipsum.
+      </p>
+    </div>
     </div>
   )
 }
